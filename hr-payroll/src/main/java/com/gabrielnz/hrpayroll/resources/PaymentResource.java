81bbc87cc1536bf637cc03744b1dc18f345c57e3
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-05-13 19:30:15 -0300

    hr-payroll implemention mock

---
+++ hr-payroll/src/main/java/com/gabrielnz/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,22 @@
+package com.gabrielnz.hrpayroll.resources;
+
+import com.gabrielnz.hrpayroll.entities.Payment;
+import com.gabrielnz.hrpayroll.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentResource {
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("/{id}/days/{days}")
+    public ResponseEntity<Payment> findById(@PathVariable Long id, @PathVariable Integer days) {
+        return ResponseEntity.ok(paymentService.getPayment(id,days));
+    }
+}
---
+++ hr-payroll/src/main/java/com/gabrielnz/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.gabrielnz.hrpayroll.services;
+
+import com.gabrielnz.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(Long workerId, int days) {
+        return new Payment("Bob", 200.00, days);
+    }
+
+}
