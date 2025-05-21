5d75cbbaa9f4e1d3aec77e3eeafaaa5293f6460a
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-05-21 15:25:09 -0300

    Private git repository (config)

--- hr-config-server/src/main/java/com/gabrielnz/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/gabrielnz/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.gabrielnz.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,7 +9,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
-
+    @Value("${spring.cloud.config.server.git.username}")
+    private String username;
     public static void main(String[] args) {
         SpringApplication.run(HrConfigServerApplication.class, args);
     }
