c0c75058c206f443a788a0cb66a497b2f6bb2729
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-05-21 15:03:55 -0300

    Refreshing configs via worker endpoint (actuator)

---
+++ course-config/src/test/java/com/gabrielnz/courseconfig/CourseConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielnz.courseconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
