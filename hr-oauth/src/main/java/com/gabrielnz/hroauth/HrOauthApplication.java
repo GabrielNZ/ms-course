3bcaeb10f79be442c4e53b391ec25d2316132350
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-05-23 13:58:42 -0300

    Configuring hr-oauth project

--- hr-oauth/src/main/java/com/gabrielnz/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/gabrielnz/hroauth/HrOauthApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HrOauthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrOauthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrOauthApplication.class, args);
+    }
 
 }
