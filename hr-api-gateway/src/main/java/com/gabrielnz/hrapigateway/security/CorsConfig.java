799928dd2dfb31481f7c9666e21ad0348ea0aba1
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-05-26 18:15:04 -0300

    CORS configuration

---
+++ hr-api-gateway/src/main/java/com/gabrielnz/hrapigateway/security/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.gabrielnz.hrapigateway.security;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica CORS para todas as rotas do gateway
+                .allowedOrigins("http://localhost:3000") // ou "*"
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
